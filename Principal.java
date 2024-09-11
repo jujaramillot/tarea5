2c67be43145de3f9d3cf367e745401e59654f042
jujaramillot <160675269+jujaramillot@users.noreply.github.com>
2024-09-10 19:10:04 -0500

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,8 @@
+package Ejercicio1;
+
+public class Principal {
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.show(true);
+    }
+}
---
+++ Tarea_5.java
@@ -0,0 +1,16 @@
+package ejercicio2;
+
+public class Tarea_5 {
+
+    public static void main(String[] args) {
+       VentanaPrincipal miVentanaPrincipal; /* Define la ventana
+       principal */ 
+       miVentanaPrincipal= new VentanaPrincipal(); /* Crea la ventana
+       principal */
+       miVentanaPrincipal.setVisible(true); /* Establece la ventana
+       como visible */
+       // Establece que la ventana no puede cambiar su tamaño
+       miVentanaPrincipal.setResizable(false);
+ 
+    }
+}
